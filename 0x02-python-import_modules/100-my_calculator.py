0a48dc5bb70aa337aa768eec12dff85458ffe2e9
Philip Obiri <philipobiri3.1@gmail.com>
2023-11-30 23:52:56 +0000

    latest fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
-def calculator(a, operator, b):
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    print(f"{a} {operator} {b} = {result}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    try:
-        a = int(sys.argv[1])
-        operator = sys.argv[2]
-        b = int(sys.argv[3])
-    except ValueError:
-        print("Invalid input. Arguments must be integers.")
-        sys.exit(1)
-
-    calculator(a, operator, b)
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for index, operator in enumerate(ops):
+        if argv[2] == operator:
+            print("{} {} {} = {}".format(a, operator, b, funcs[index](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators are: +, -, * and /")
+        quit(1)
