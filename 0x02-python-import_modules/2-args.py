a4542d6e97fe865326e58d2c6a6becb0e8b86c8c
Philip Obiri <philipobiri3.1@gmail.com>
2023-11-30 23:17:08 +0000

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-def print_command_line_arguments():
-    number_of_command_line_arguments = len(sys.argv) - 1
-    if number_of_command_line_arguments == 0:
-        print("0 arguments.")
-    elif number_of_command_line_arguments == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(number_of_command_line_arguments))
-    for i in range(number_of_command_line_arguments):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+number_of_command_line_arguments = len(sys.argv) - 1
+if number_of_command_line_arguments == 0:
+    print("0 arguments.")
+elif number_of_command_line_arguments == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(number_of_command_line_arguments))
+for i in range(number_of_command_line_arguments):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
 if __name__ == "__main__":
-    print_command_line_arguments()
+    """Print the number of command line arguments (normally in a list form)."""
