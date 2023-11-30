95aa4e46c282d5762677cb1e2001db877378faf0
Philip Obiri <philipobiri3.1@gmail.com>
2023-11-30 23:30:12 +0000

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     total = 0
-    for s in argv[1:]:
-        total += int(s)
+    for user_input in argv[1:]:
+        total += int(user_input)
     print("{:d}".format(total))
