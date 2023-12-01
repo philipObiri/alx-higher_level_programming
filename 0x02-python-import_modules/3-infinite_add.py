c775054307b95d2b448ce5d5b5fba4bede456b17
Philip Obiri <53437017+philipObiri@users.noreply.github.com>
2023-12-01 10:47:21 +0000

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    total = 0
-    for user_input in argv[1:]:
-        total += int(user_input)
-    print("{:d}".format(total))
